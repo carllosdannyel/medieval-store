b97602e62760b1f8ea795cc48c5bee6c783be1c8
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-11-04 14:17:29 -0300

    iniciando o projeto

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import app from './app';
 
 const PORT = 3000;
+console.log(PORT);
 
-const server = app.listen(PORT, () => console.log(
-  `Server is running on PORT: ${PORT}`,
-));
+const server = app.listen(PORT, () =>
+  console.log(`Server is running on PORT: ${PORT}`),
+);
 
 export default server;
