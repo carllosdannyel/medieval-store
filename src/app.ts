2dca0a05bef6b1db84ebb8d2534597a3220315d2
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-11-07 05:13:26 -0300

    descomenta codigo do app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-// import 'express-async-errors';
-// import errorMiddleware from './middlewares/error';
+import 'express-async-errors';
+import errorMiddleware from './middlewares/error';
 import productRoutes from './routes/product';
 
 const app = express();
@@ -8,5 +8,5 @@ app.use(express.json());
 
 app.use('/products', productRoutes);
 
-// app.use(errorMiddleware);
+app.use(errorMiddleware);
 export default app;
